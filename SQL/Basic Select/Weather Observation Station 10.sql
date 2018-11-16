d44e7b18d8613f140944368a59584a8a76e78ec6
botsteve <rusenstefan15@yahoo.com>
2018-11-16 23:05:29 +0200

    New solutions

---
+++ SQL/Basic Select/Weather Observation Station 10.sql
@@ -0,0 +1,11 @@
+--     Author: Rusen Stefan
+--     Github: github.com/botsteve
+-- HackerRank: hackerrank.com/rusenstefan15
+
+SELECT DISTINCT CITY FROM STATION WHERE CITY REGEXP '[^aeiou]$';
+--  Pattern   | What the pattern matches
+---------------------------------------
+--    $       |  End of string
+--    .       |  Matches any character
+--    *	      |  Matches the previous char for 0 or more times	
+--  [...]     |  Any character listed between the square brackets
